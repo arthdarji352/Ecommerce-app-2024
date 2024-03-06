1bafa527715be7e7d2eb2d401d8dfa74baf954fa
arthdarji352 <arthdarji20@gmail.com>
2024-03-07 00:13:23 +0530

    add products api slice

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Product from "../components/Product";
-import axios from "axios";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+  const { data: products, isLoading, error } = useGetProductsQuery();
+  // console.log(products);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-    </div>
+    <>
+      {isLoading && <h1>Loading...</h1>}
+      {error && <div>{error?.data?.message || error?.error}</div>}
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4">
+        {products?.map((product, i) => (
+          <Product product={product} />
+        ))}
+      </div>
+    </>
   );
 };
 
--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -1,20 +1,16 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 export default function ProductScreen() {
-  const [product, setProduct] = useState({});
+  const { id: productId } = useParams();
 
-  const { id } = useParams();
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId);
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${id}`);
-      setProduct(data);
-    };
-    fetchProduct();
-  }, [id]);
-
+  // console.log(product);
   return (
     <div className="container mx-auto mt-8 p-4">
       <Link to={"/"}>
@@ -22,45 +18,52 @@ export default function ProductScreen() {
           Go Back
         </button>
       </Link>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="md:col-span-1">
-          <img
-            src={product.image}
-            alt={product.name}
-            className="w-full h-auto"
-          />
-        </div>
-        <div className="md:col-span-1">
-          <h1 className="text-2xl font-semibold">{product.name}</h1>
-          <p className="text-gray-700 mt-2">{product.description}</p>
-          <div className="flex items-center mt-2">
-            <span className="text-yellow-500 mr-1">{product.rating}</span>
-            <span className="text-gray-500">
-              ({product.numReviews} reviews)
-            </span>
+
+      {isLoading && <h1>Loading...</h1>}
+      {error && <div>{error?.data?.message || error?.error}</div>}
+      {product && (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="md:col-span-1">
+            <img
+              src={product.image}
+              alt={product.name}
+              className="w-full h-auto"
+            />
           </div>
-          <p className="text-gray-700 mt-2">${product?.price?.toFixed(2)}</p>
-          <p className="text-gray-700 mt-2">In Stock: {product.countInStock}</p>
-          <div className="mt-4">
-            <label htmlFor="quantity" className="text-gray-700">
-              Quantity:
-            </label>
-            <select
-              id="quantity"
-              className="bg-white border border-gray-300 p-2 rounded-md mt-2"
-            >
-              {[...Array(product.countInStock).keys()].map((num) => (
-                <option key={num + 1} value={num + 1}>
-                  {num + 1}
-                </option>
-              ))}
-            </select>
+          <div className="md:col-span-1">
+            <h1 className="text-2xl font-semibold">{product.name}</h1>
+            <p className="text-gray-700 mt-2">{product.description}</p>
+            <div className="flex items-center mt-2">
+              <span className="text-yellow-500 mr-1">{product.rating}</span>
+              <span className="text-gray-500">
+                ({product.numReviews} reviews)
+              </span>
+            </div>
+            <p className="text-gray-700 mt-2">${product?.price?.toFixed(2)}</p>
+            <p className="text-gray-700 mt-2">
+              In Stock: {product.countInStock}
+            </p>
+            <div className="mt-4">
+              <label htmlFor="quantity" className="text-gray-700">
+                Quantity:
+              </label>
+              <select
+                id="quantity"
+                className="bg-white border border-gray-300 p-2 rounded-md mt-2"
+              >
+                {[...Array(product.countInStock).keys()].map((num) => (
+                  <option key={num + 1} value={num + 1}>
+                    {num + 1}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button className="bg-yellow-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-yellow-600">
+              Add to Cart
+            </button>
           </div>
-          <button className="bg-yellow-500 text-white px-4 py-2 rounded-md mt-4 hover:bg-yellow-600">
-            Add to Cart
-          </button>
         </div>
-      </div>
+      )}
     </div>
   );
 }
