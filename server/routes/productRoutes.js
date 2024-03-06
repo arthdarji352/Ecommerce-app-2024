f70c614fa44727cb5842b7fc03a63d1e801b54a1
arthdarji352 <arthdarji20@gmail.com>
2024-03-06 14:11:17 +0530

    add custom errorHandler middleware

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,23 +1,12 @@
 import express from "express";
-import Product from "../models/productModel.js";
-import asyncHandler from "express-async-handler";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    res.json(product);
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -17,6 +18,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
