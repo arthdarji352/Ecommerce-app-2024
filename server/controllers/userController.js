09d17ed64a41b3498414fd44d7f1ff30244fbfb2
arthdarji352 <arthdarji20@gmail.com>
2024-03-09 13:35:01 +0530

    add register controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 import asyncHandler from "express-async-handler";
 import UserModel from "../models/userModel.js";
 import { generateToken } from "../config/generateToken.js";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -21,4 +23,37 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { loginUser };
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await UserModel.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already Exists");
+  }
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  if (user) {
+    generateToken(res, user._id);
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Credentials");
+  }
+});
+
+export { loginUser, registerUser };
